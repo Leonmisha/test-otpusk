bc554f25e9f598344b2eb47d6a8101ce3e118185
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2021-07-08 08:56:06 +0300

    fix baseName for gh-pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
